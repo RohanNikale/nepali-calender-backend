ed4aa7d9a154ed3445c3316d121d1710d2a6186d
rohannikale <rohanpadit745@gmail.com>
2023-08-14 16:02:03 +0530

    successfully created setup project and file structure

---
+++ Utils/sendOtp.js
@@ -0,0 +1,21 @@
+const { Vonage } = require('@vonage/server-sdk');
+
+const vonage = new Vonage({
+    apiKey: proccess.env.SMS_API_KEY,
+    apiSecret: proccess.env.SECRET_API_KEY
+});
+
+const sendOtp = async (number, otp) => {
+    const from = "Vonage APIs";
+    const to = number;
+    const text = `Your OTP is ${otp}`;
+
+    try {
+        const response = await vonage.sms.send({ to, from, text });
+        console.log('Message sent successfully:', response);
+    } catch (error) {
+        console.error('Error sending the message:', error);
+    }
+};
+
+module.exports = sendOtp;
---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv')
+
+const app = express();
+const bodyParser = require('body-parser');
+dotenv.config({path:'./Config/config.env'})
+
+// Middlewares
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/',(req,res)=>{
+    res.send('hello world')
+})
+// Route imports
+// const user = require('./routes/userRoutes');
+const authRoutes = require('./routes/authRoutes')
+
+// app.use('/api/v1',user);
+app.use('/', authRoutes);
+
+
+// Error Middleware
+
+module.exports = app;
