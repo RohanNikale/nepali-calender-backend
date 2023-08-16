b71798061ccaa2a6808243d1bb287a88a47e4e63
rohannikale <rohanpadit745@gmail.com>
2023-08-16 19:47:00 +0530

    created update event oparetion endpoint api

--- Controller/eventController.js
+++ Controller/eventController.js
@@ -17,3 +17,25 @@ exports.createEvent = async (req, res, next) => {
         res.status(404).json(error)
     }
 };
+
+exports.updateEvent = async (req, res, next) => {
+    const { eventid } = req.headers;
+    
+    try {
+        const findEvent = await Event.findById(eventid);
+        if (!(findEvent.userid == req.user._id)) {
+            return res.status(404).json({ status: false, message: 'this user does not have access to this event' });
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(eventid, req.body, { new: true });
+
+        if (!updatedEvent) {
+            return res.status(404).json({ status: false, message: 'Event not found' });
+        }
+
+        res.status(200).json({ status: true, message: 'Document updated successfully', updatedEvent });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ status: false, message: 'An error occurred' });
+    }
+};
