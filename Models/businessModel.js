25d7a6b56f2c99e54b220c028712713deaafbac0
rohannikale <rohanpadit745@gmail.com>
2023-08-22 14:36:39 +0530

    created business CRUD

--- Models/businessModel.js
+++ Models/businessModel.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const businessSchema = new mongoose.Schema({
+  userId:{
+    type: String,
+    required: true
+  },
   businessName: {
     type: String,
     required: true
