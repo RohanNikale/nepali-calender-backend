080562a1b948d4a709eb2ed3848ff644c4873d91
rohannikale <rohanpadit745@gmail.com>
2023-08-22 14:36:39 +0530

    created business CRUD

---
+++ Controller/businessController.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const Business = require('../Models/businessModel');
+
+// Endpoint for creating a new business
+exports.createBusiness = async (req, res) => {
+    try {
+        const newBusiness = new Business(
+            {
+                userId:req.user.id,
+            ...req.body});
+
+        await newBusiness.save();
+
+        res.status(201).json({
+            message: 'Successfully created business',
+            status: true
+        });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+// Base function for updating and deleting a business
+async function modifyBusiness(req, res, action) {
+    const businessId = req.params.businessid;
+
+    try {
+        const findBusiness = await Business.findById(businessId);
+
+        if (!findBusiness) {
+            return res.status(404).json({ status: false, message: 'Business not found' });
+        }
+
+        const result = await action(businessId, req.body);
+
+        if (!result) {
+            return res.status(404).json({ status: false, message: 'Business not found' });
+        }
+
+        res.status(200).json({ status: true, message: 'Operation successful', result });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ status: false, message: 'An error occurred', error });
+    }
+}
+
+// Endpoint for updating a business
+exports.updateBusiness = (req, res) => modifyBusiness(req, res, async (businessId, data) => {
+    return await Business.findByIdAndUpdate(businessId, data, { new: true });
+});
+
+// Endpoint for deleting a business
+exports.deleteBusiness = (req, res) => modifyBusiness(req, res, async (businessId) => {
+    return await Business.findByIdAndDelete(businessId);
+});
+
+// Endpoint for getting business data
+exports.getBusinessData = async (req, res) => {
+    try {
+        const businessId = req.params.businessid;
+
+        const business = await Business.findById(businessId);
+
+        if (!business) {
+            return res.status(404).json({ status: false, message: 'Business not found' });
+        }
+
+        res.status(200).json({ status: true, business });
+    } catch (error) {
+        res.status(500).json({ status: false, message: 'An error occurred', error });
+    }
+};
--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const { createBusiness, updateBusiness, deleteBusiness, getBusinessData } = require('../Controller/BusinessController');
+const { createBusiness, updateBusiness, deleteBusiness, getBusinessData } = require('../Controller/businessController');
 const { isAuthenticatedAdmin } = require('../Middleware/adminAuth');
 
 // Create Event
 router.post('/createBusiness', isAuthenticatedAdmin, createBusiness);
 
 // Update Event
-router.put('/updateBusiness', isAuthenticatedAdmin, updateBusiness);
+router.put('/updateBusiness/:businessid', isAuthenticatedAdmin, updateBusiness);
 
 // Delete Event
-router.delete('/deleteBusiness', isAuthenticatedAdmin, deleteBusiness);
+router.delete('/deleteBusiness/:businessid', isAuthenticatedAdmin, deleteBusiness);
 
 // Read Event
-router.get('/getBusinessdata', getBusinessData);
+router.get('/getBusinessdata/:businessid', getBusinessData);
 
 module.exports = router;
