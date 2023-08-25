df133f246414cb4199be6ac44919952be14b9759
rohannikale <rohanpadit745@gmail.com>
2023-08-25 14:46:05 +0530

    change otp provider

--- Controller/authController.js
+++ Controller/authController.js
@@ -37,9 +37,7 @@ exports.SignUpLoginByOtp = async (req, res, next) => {
         }
 
         // Send the OTP here
-        // sendOtp(number, otp);
-
-        console.log({ otp });
+        sendOtp(number, otp);
 
         res.status(201).json({
             message: "The 5-digit OTP has been sent to your phone number",
--- Utils/sendOtp.js
+++ Utils/sendOtp.js
@@ -1,21 +1,23 @@
-const { Vonage } = require('@vonage/server-sdk');
+const twilio = require('twilio');
 
-const vonage = new Vonage({
-    apiKey: process.env.SMS_API_KEY,
-    apiSecret: process.env.SECRET_API_KEY
-});
+// Your Twilio Account SID and Auth Token
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 
-const sendOtp = async (number, otp) => {
-    const from = 'Vonage APIs';
-    const to = number;
-    const text = `Your OTP is ${otp}`;
+// Create a Twilio client instance
+const client = new twilio(accountSid, authToken);
 
-    try {
-        const response = await vonage.sms.send({ to, from, text });
-        console.log('Message sent successfully:', response);
-    } catch (error) {
-        console.error('Error sending the message:', error);
-    }
-};
+// Function to send an OTP via SMS
+function sendOtp(recipientPhoneNumber,otp) {
+    const message = `Your OTP is: ${otp} Do Not Share This code with anyone`;
+
+    return client.messages.create({
+        body: message,
+        from: '+18146664052',
+        to: recipientPhoneNumber
+    });
+}
+
+// Example usage
 
 module.exports = sendOtp;
